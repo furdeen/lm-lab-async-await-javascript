de93d1e39e937e041416b794ac2340f6bafa0366
Faisal Mumtaz <fascaptech@gmail.com>
2023-11-08 09:49:39 +0000

    completed the excrise1 and exercise 2

--- exercise001.js
+++ exercise001.js
@@ -12,3 +12,10 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // Your solution(s) to exercise001 here!
+promise
+.then((Response) => {
+	console.log('Yay! Promise resolved with response: done')
+})
+.catch((reject)=>{
+	console.log('Boo. Promise rejected with response. error')
+});
--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,23 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
-
+//const fetchData = (apiEndPoint) => {
+  //fetch(apiEndPoint)
+   // .then((response) => response.json())
+    //.then((json) => console.log(json))
+    //.catch((error) => console.log(error));
+//};
+const fetchData =async (apiEndPoint) => {
+  try
+  {  const response =await fetch(apiEndPoint);
+     if(!response.ok){
+       throw new error(' HTTP ERROR!');
+       }  
+    const json = await response.json();
+    console.log(json);
+     }
+     catch(error){
+     console.log('An Error Occured',error);
+     }
+ };
 fetchData(jsonTypicode);
